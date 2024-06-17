04cb1e4bda02f80ef34cec058835b0165747d972
iamshaunjp <shaun@thenetninja.co.uk>
2024-06-17 10:28:37 +0100

    lesson-2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import articles from '../data/articles.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Product Listing'});
+  res.render('index', {title: 'Product Listing', articles: articles});
 });
 
 export default router;
