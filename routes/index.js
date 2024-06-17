1be8b3fc73a0a1e3fb910ff642b5c10d4f08733e
Shaun (Net Ninja) <9838872+iamshaunjp@users.noreply.github.com>
2024-06-17 11:32:07 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   res.render('index', {title: 'Product Listing', articles: articles});
 });
 
-router.get('/article/:id', (req, res) => {
+router.get('/articles/:id', (req, res) => {
   const article = articles.find(a => a.id === parseInt(req.params.id));
 
   res.render('article', { title: article.name, article });
