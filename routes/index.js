c016dabca1133b1154f68cf2f20435dd1c14b3ea
iamshaunjp <shaun@thenetninja.co.uk>
2024-06-17 11:31:07 +0100

    lesson-5

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,23 @@ router.get('/', (req, res) => {
   res.render('index', {title: 'Product Listing', articles: articles});
 });
 
-router.get('/article/:id', (req, res) => {
+router.get('/articles/:id', (req, res) => {
   const article = articles.find(a => a.id === parseInt(req.params.id));
 
   res.render('article', { title: article.name, article });
+});
+
+router.post('/articles', (req, res) => {
+  const { name, body } = req.body;
+
+  const article = {
+    id: Math.floor(Math.random() * 1000000),
+    name,
+    body
+  };
+  articles.push(article);
+
+  res.render('partials/list', { articles });
 });
 
 export default router;
